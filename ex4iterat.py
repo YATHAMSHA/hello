e6581c4ce2041ae8f3d4341d571bab7845ea6ad6
YATHAM SHAVALI <yatham024@gmail.com>
2022-10-09 13:03:12 +0530

    iterator added

---
+++ ex4iterat.py
@@ -0,0 +1,11 @@
+string = "YATHAM"
+
+for element in string:
+    print(element, end=" ")
+print("\n")
+
+
+string_name = "yatham"
+
+for element in range(0, len(string_name)):
+    print(string_name[element])
